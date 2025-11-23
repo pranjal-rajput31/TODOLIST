872abcbf21e634910ec16218bf71398633500063
pranjal-rajput31 <pranjalrajput31102003@gmail.com>
2025-11-23 19:12:32 +0530

    Add Header component for to-do list title

---
+++ Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// Header component to display the title of the to-do list
+const Header = () => {
+  return <h1 className="header"> 📝 My To-Do List</h1>;
+};
+
+export default Header;
